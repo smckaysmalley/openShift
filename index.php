84b2cd1cbb70abc9d24eaf59d3724f2f4cb1104e
smckaysmalley <smckaysmalley@gmail.com>
2015-01-06 16:05:13 -0700

    for real this time

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
-<?php require( $_SERVER['DOCUMENT_ROOT'] . '/header.php'); ?>
-		<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
-			<div class="panel panel-primary">
-				<div class="panel-heading">
-					<div class="panel-title">Welcome to McKay Smalley's Home Page</div>
-				</div>
-				<div class="panel-body">
-					<h1>Hello World!</h1>
-				</div>
-			</div>
-		</div>				
-		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 center">
-			<iframe src="https://onedrive.live.com/embed?cid=2FBC18EAF6E2F93E&resid=2FBC18EAF6E2F93E%211604&authkey=AAdE9TBWAR_OsG8" width="319" height="180" frameborder="0" scrolling="no"></iframe>
-		</div>
-<?php require( $_SERVER['DOCUMENT_ROOT'] . '/footer.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+	<meta http-equiv="refresh" content="20; url=http://mckaysmalley.com/" />
+</head>
+<body>
+	<h1>Hello World</h1>
+</body>
+</html>
