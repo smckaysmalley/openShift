c57b88f7f1b71770e19e07fe999c3748a77d6637
smckaysmalley <smckaysmalley@gmail.com>
2015-01-08 18:34:39 -0700

    yep

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Hello World</title>
 	<meta http-equiv="refresh" content="20; url=http://mckaysmalley.com/" />
+	<link rel="stylesheet" href="css/general.css" type="text/css"/>
 </head>
 <body>
 	<h1>Hello World</h1>
