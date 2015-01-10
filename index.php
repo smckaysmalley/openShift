6bd8996acc21ffecdc3c1ac725db70efaafec452
smckaysmalley <smckaysmalley@gmail.com>
2015-01-10 15:32:19 -0700

    commit

--- index.php
+++ index.php
@@ -7,9 +7,12 @@
 </head>
 <body>
 	<h1>Hello World</h1>
+<<<<<<< HEAD
 	<p><?php 
             echo "Right now it is " . strftime(%A, %B, %d, %Y, time()) . " where my server is."
         ?>
     </p>
+=======
+>>>>>>> parent of 339fed7... Used some php
 </body>
 </html>
