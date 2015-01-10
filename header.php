339fed775f8fce480b74302f06ee43b1fba75202
smckaysmalley <smckaysmalley@gmail.com>
2015-01-10 14:21:10 -0700

    Used some php

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="utf-8" http-equiv="encoding">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="McKay Smalley">
+    <base href="http://php-smckaysmalley.rhcloud.com/"></base>
+    <title>McKay Smalley</title>
+    <link rel="stylesheet" type="text/css" href="css/general.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="shortcut icon" href="images/icon.png">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/general.js"></script>
+</head>
+<body>
+    
--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 </head>
 <body>
 	<h1>Hello World</h1>
+	<p><?php 
+            echo "Right now it is " . time() . " where my server is."
+        ?>
+    </p>
 </body>
 </html>
