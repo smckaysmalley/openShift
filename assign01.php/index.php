1b42f5b06ddfeeed4d1096fa2d6a59071a3e8565
smckaysmalley <smckaysmalley@gmail.com>
2015-01-12 14:27:48 -0700

    changed home page

---
+++ assign01.php/index.php
@@ -0,0 +1,3 @@
+<?php require( $_SERVER['DOCUMENT_ROOT'] . '/header.php'); ?>
+		<h1>Hello World</h1>
+<?php require( $_SERVER['DOCUMENT_ROOT'] . '/footer.php'); ?>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php require( $_SERVER['DOCUMENT_ROOT'] . '/header.php'); ?>
-		<h1>Hello World</h1>
+		<div class="panel panel-primary">
+		    <div class="panel-heading">
+		        <div class="panel-title">
+		            McKay's home page
+		        </div>
+		    </div>
+		    <div class="panel-body">
+		        <h1>HELLO WORLD!</h1>
+		    </div>
+		</div>
 <?php require( $_SERVER['DOCUMENT_ROOT'] . '/footer.php'); ?>
