0a95e4fb7352cbe35b42481d898e4782efcc1ca4
smckaysmalley <smckaysmalley@gmail.com>
2015-01-24 16:55:47 -0700

    enhancing header
    icon shortcut

--- header.php
+++ header.php
@@ -6,8 +6,12 @@
     <title>McKay Smalley</title>
     <link rel="stylesheet" href="http://mckaysmalley.com/css/bootstrap.min.css" type="text/css" />
     <link rel="stylesheet" href="/css/general.css" type="text/css" />
-    <link rel="shortcut icon" href="mountain-128.png">
-    <!-- <base href="http://php-smckaysmalley.rhcloud.com/"></base> -->
+    <link rel="shortcut icon" href="/images/mountain-128.png">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+	<meta content="utf-8" http-equiv="encoding">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="McKay Smalley">
 </head>
 
 <body>
