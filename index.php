b3ae48f4a35604910c72acf90b6770a84d3da699
smckaysmalley <smckaysmalley@gmail.com>
2015-01-26 08:53:57 -0700

    mobile changes

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php require( $_SERVER[ 'DOCUMENT_ROOT'] . '/header.php'); ?>
-<link rel="stylesheet" type="text/css" href="/css/index.css">
+<link rel="stylesheet" type="text/css" href="/css/index.css" />
 
 <section id="section1">
-    <div class="slider" data--0-bottom="background-position:50% 70%; background-size:140%;" data-0-top="background-position:50% 30%; background-size:100%;">
-        <div class="info-box" data--25-bottom="left:-250px;" data--75-bottom="left:0px" data-60-top="left:0px" data-10-top="left:-250px;">
+    <div class="slider" data--0-bottom="background-position:50% 70%; background-size:140%;" data-0-top="background-position:50% 30%; background-size:110%;">
+        <div class="info-box" data-bottom="left:-20%;" data--40-bottom="left:0%" data-40-top="left:0%" data-top="left:-20%;">
             <div class="box-title">About</div>
             <div class="box-content">
                 My name is McKay Smalley. I am a Comuter Science student at Brigham Young University-Idaho. I grew up in Summit, New Jersey (about 20-30 minutes from NYC).
@@ -14,28 +14,30 @@
 
 <section class="content fly-in-left">
     <div class="text">
-        Currently I am a Web Developer for the Student Support and Activities Committee on campus. 
+        Currently I am a Web Developer for the Student Services and Activities Committee on campus.
     </div>
 </section>
 
 <section id="section2">
     <div class="slider" data--0-bottom="background-position:0% 70%; background-size: 110%" data-0-top="background-position:50% 30%;background-size: 140%">
-        <div class="info-box" data--25-bottom="left:-250px;" data--75-bottom="left:0px" data-75-top="left:0px" data-25-top="left:-250px;">
+        <div class="info-box" data-bottom="left:-20%;" data--40-bottom="left:0%" data-40-top="left:0%" data-top="left:-20%x;">
             <div class="box-title">Skiing</div>
-            <div class="box-content">My Wife and I both enjoy skiing. This picture was from our recent trip to Alta on December 30, 2014. It was a cold 0&deg; that day.</div>
+            <div class="box-content">
+                My Wife and I both enjoy skiing. This picture was from our recent trip to Alta on December 30, 2014. It was a cold 0&deg; that day.
+            </div>
         </div>
     </div>
 </section>
 
 <section class="content fly-in-right">
     <div class="text">
-        I married my beautiful wife on September 27, 2013 in Idaho Falls, Idaho. 
+        I married my beautiful wife on September 27, 2013 in Idaho Falls, Idaho.
     </div>
 </section>
 
 <section id="section3">
-    <div class="slider" data-0-bottom="background-position:50% 20%; background-size:120%;" data-0-top="background-position:50% 30%;  background-size: 100%;">
-        <div class="info-box" data--25-bottom="left:-250px;" data--75-bottom="left:0px" data-75-top="left:0px" data-25-top="left:-250px;">
+    <div class="slider" data-0-bottom="background-position:50% 20%; background-size:120%;" data-0-top="background-position:50% 30%;  background-size: 110%;">
+        <div class="info-box" data-bottom="left:-20%;" data--40-bottom="left:0%" data-40-top="left:0%" data-top="left:-20%;">
             <div class="box-title">Mountain Biking</div>
             <div class="box-content">
                 I have been mountain biking since I was 10. I enjoy finding new trails. I love to be up in the mountains.
@@ -46,7 +48,7 @@
 
 <section class="content fly-in-left">
     <div class="text">
-        I served my mission in Colorado Springs, Colorado. I loved my mission. 
+        I served my mission in Colorado Springs, Colorado. I loved my mission.
     </div>
 </section>
 
