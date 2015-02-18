bcbc67825939f09ebb0d3bd32f4e7b71ae02b914
smckaysmalley <smckaysmalley@gmail.com>
2015-02-17 17:57:19 -0700

    good changes all around

--- valiant_11/comment.php
+++ valiant_11/comment.php
@@ -10,7 +10,7 @@ if (isset($_POST))
     $insert_query = $valiant_db->prepare("INSERT INTO comment (content, parent, commented_by, creation_date) VALUES (:comment, :parent, :user, NOW())");
     $insert_query->execute(array(':comment' => $comment, ':parent' => $_POST['parent'], ':user' => $_POST['user']));
 
-    echo "<div class='comment-box'>" . $_POST['comment'] . "</div>";
+    echo "<div class='comment-box'>" . $_SESSION['firstname'] . ': ' . $_POST['comment'] . "</div>";
     
     //close the connection
     $valiant_db = null;
