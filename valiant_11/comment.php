fdc28fab493a69c10691abf479bc07f9ecdf85b6
smckaysmalley <smckaysmalley@gmail.com>
2015-02-18 23:17:08 -0700

    more testing

--- valiant_11/comment.php
+++ valiant_11/comment.php
@@ -12,32 +12,32 @@ if (isset($_POST['comment']))
     $insert_query = $valiant_db->prepare("INSERT INTO comment (content, parent, commented_by, creation_date) VALUES (:comment, :parent, :user, NOW())");
     $insert_query->execute(array(':comment' => $comment, ':parent' => $_POST['parent'], ':user' => $_POST['user']));
     
-    echo 1;
-    
-    //find out if anyone else has commented since the page has loaded
-    $count_query = "SELECT count(*) as 'count' FROM comment WHERE parent = " . $_POST['parent'];
-    $count_result = $valiant_db->query($count_query);
-    $count_row = $count_result->fetch(PDO::FETCH_ASSOC);
-    
-    echo 2;
-    
-    //get the difference
-    $limit = $count_row['count'] - $_POST['count'];
-    
-    echo 3;
-    
-    //returns all new comments
-    $comment_query = "SELECT temp.content FROM (SELECT content, creation_date FROM comment c WHERE parent = " . $_POST['parent'] . " ORDER BY creation_date DESC LIMIT " . $limit . ") temp ORDER BY temp.creation_date ASC";
-    $comment_result = $valiant_db->query($comment_query);
-    
-    echo 4;
-    
-    while($comment_row = $comment_result->fetch(PDO::FETCH_ASSOC))
-    {
-        echo "<div class='comment-box'>" . $comment_row['content'] . "</div>";
-    }
-    
-    echo 5;
+//    echo 1;
+//    
+//    //find out if anyone else has commented since the page has loaded
+//    $count_query = "SELECT count(*) as 'count' FROM comment WHERE parent = " . $_POST['parent'];
+//    $count_result = $valiant_db->query($count_query);
+//    $count_row = $count_result->fetch(PDO::FETCH_ASSOC);
+//    
+//    echo 2;
+//    
+//    //get the difference
+//    $limit = $count_row['count'] - $_POST['count'];
+//    
+//    echo 3;
+//    
+//    //returns all new comments
+//    $comment_query = "SELECT temp.content FROM (SELECT content, creation_date FROM comment c WHERE parent = " . $_POST['parent'] . " ORDER BY creation_date DESC LIMIT " . $limit . ") temp ORDER BY temp.creation_date ASC";
+//    $comment_result = $valiant_db->query($comment_query);
+//    
+//    echo 4;
+//    
+//    while($comment_row = $comment_result->fetch(PDO::FETCH_ASSOC))
+//    {
+//        echo "<div class='comment-box'>" . $comment_row['content'] . "</div>";
+//    }
+//    
+//    echo 5;
     
     //close the connection
     $valiant_db = null;
